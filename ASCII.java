6935f6fe9382902da3518d4d085858c5e9f4ba7c
kaarthigaG <114210312+kaarthigaG@users.noreply.github.com>
2022-09-23 11:45:11 +0530

    Add files via upload
    I have just written a code to find the value of ASCII values in java

---
+++ ASCII.java
@@ -0,0 +1,8 @@
+public class ASCII {
+    public static void main(String[] args) {
+        int chr = 'k';
+        int chr1 = 'r';
+        System.out.println("the ASCII value of k is:" +chr);
+        System.out.println("the ASCII value of r is:" +chr1);
+    }   
+}
